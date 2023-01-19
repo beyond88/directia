b3e8682f997ed9ec6189cfd50d448a3a1f2fe0ff
beyond88 <muhin.cse.diu@gmail.com>
2023-01-19 11:29:34 +0600

    listing details page created for client area

--- includes/Installer.php
+++ includes/Installer.php
@@ -15,6 +15,7 @@ class Installer {
     public function run() {
         $this->addVersion();
         $this->createTables();
+        $this->setupPages();
     }
 
     /**
@@ -55,5 +56,95 @@ class Installer {
         dbDelta( $sql );
         $is_error = empty( $wpdb->last_error );
         return $is_error;
+    }
+
+    /**
+     * Setup all pages for directia
+     *
+     * @return void
+     */
+    public function setupPages() {
+        $meta_key = '_wp_page_template';
+
+        // return if pages were created before
+        $page_created = get_option( 'directia_pages_created', false );
+
+        if ( $page_created ) {
+            return;
+        }
+
+        $pages = [
+            [
+                'post_title' => __( 'Directia Listing Details', 'directia' ),
+                'slug'       => 'directia-listing-details',
+                'page_id'    => 'directia_listing_details',
+                'content'    => '[directia_listing_details]',
+            ]
+        ];
+
+        $dokan_page_settings = [];
+
+        if ( $pages ) {
+            foreach ( $pages as $page ) {
+                $page_id = $this->createPage( $page );
+
+                if ( $page_id ) {
+                    $dokan_page_settings[ $page['page_id'] ] = $page_id;
+
+                    if ( isset( $page['child'] ) && count( $page['child'] ) > 0 ) {
+                        foreach ( $page['child'] as $child_page ) {
+                            $child_page_id = $this->createPage( $child_page );
+
+                            if ( $child_page_id ) {
+                                $dokan_page_settings[ $child_page['page_id'] ] = $child_page_id;
+
+                                wp_update_post(
+                                    [
+										'ID'          => $child_page_id,
+										'post_parent' => $page_id,
+									]
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        update_option( 'directia_pages', $dokan_page_settings );
+        update_option( 'directia_pages_created', true );
+    }
+
+    /**
+     * Create all listing pages
+     *
+     * @return void
+     */
+    public function createPage( $page ) {
+        $meta_key = '_wp_page_template';
+        $page_obj = get_page_by_path( $page['post_title'] );
+
+        if ( ! $page_obj ) {
+            $page_id = wp_insert_post(
+                [
+					'post_title'     => $page['post_title'],
+					'post_name'      => $page['slug'],
+					'post_content'   => $page['content'],
+					'post_status'    => 'publish',
+					'post_type'      => 'page',
+					'comment_status' => 'closed',
+				]
+            );
+
+            if ( $page_id && ! is_wp_error( $page_id ) ) {
+                if ( isset( $page['template'] ) ) {
+                    update_post_meta( $page_id, $meta_key, $page['template'] );
+                }
+
+                return $page_id;
+            }
+        }
+
+        return false;
     }
 }
